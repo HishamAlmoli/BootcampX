78583c34681bae0078fe0e85b91a4e0c6baf7ab6
HishamAlmoli <halmoli@my.centennialcollege.ca>
2022-12-17 19:25:18 -0500

    Add Student and teachers file

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,8 +1,7 @@
 const { Pool } = require('pg');
 const args = process.argv.slice(2);
 const cohortName = args[0];
-const limit = args[1];
-const values = [`%${cohortName}%`, limit];
+const values = [`%${cohortName}%`];
 
 const pool = new Pool({
   user: 'vagrant',
@@ -12,17 +11,19 @@ const pool = new Pool({
 });
 
 const queryString = `
-SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-FROM students
-JOIN cohorts ON cohorts.id = cohort_id
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
 WHERE cohorts.name LIKE $1
-LIMIT $2;
+ORDER BY teacher;
 `;
 
 pool.query(queryString, values)
 .then(res => {
-  res.rows.forEach(user => {
-    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+  res.rows.forEach(row => {
+    console.log(`${row.cohort}: ${row.teacher}`);
   })
 })
 .catch(err => console.error('query error', err.stack));
